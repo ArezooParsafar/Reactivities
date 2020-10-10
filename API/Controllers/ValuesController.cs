eea2ff10dfdfab9c9af90fb2e84c50aebddeaa25
The Wish <Arezoo.parsafar86@gmail.com>
2020-10-10 13:46:05 +0330

    Initial Commit

---
+++ API/Controllers/ValuesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        public ActionResult<IEnumerable<Value>> Get()
+        {
+            var values = _context.Values.ToList();
+        }
+    }
+}
+
